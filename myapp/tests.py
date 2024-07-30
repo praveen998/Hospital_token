b70a330d0597ce5450f54a25c418db2ee42b57fa
praveen <praveengopi998@gmail.com>
2024-07-30 15:21:15 +0530

    new commit

---
+++ myapp/tests.py
@@ -0,0 +1,19 @@
+import pytest
+from django.urls import reverse
+from django.test import Client
+from .utils import addition
+
+@pytest.mark.django_db
+def test_home_views():
+    client=Client()
+    response = client.get(reverse('home'))
+    assert response.status_code == 200
+    assert response.content.decode() == "success"
+
+def test_addition():
+    assert addition(1,2)==3
+    assert addition(2,2)==4
+    assert addition(7,2)==9
+    assert addition(1,3)==4
+    
+    
